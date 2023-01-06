d8bf394e54c56bdab4e000c138f78c4ccf444a26
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2023-01-06 17:02:54 +0000

    Add workflow

--- pkg/storage/in_memory.go
+++ pkg/storage/in_memory.go
@@ -1,5 +1,8 @@
 package storage
 
+import (
+  "github.com/rs/xid"
+)
 
 type InMemoryDB map[string]interface{}
 
@@ -7,4 +10,10 @@ type InMemoryDB map[string]interface{}
 func NewInMemoryDB() InMemoryDB {
 	MemoryDB := make(map[string]interface{})
 	return MemoryDB
+}
+
+
+func(IMDB InMemoryDB) Create(c interface{}) (string, error){
+  id := xid.New()
+  IMDB[id] = c
 }
